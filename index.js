2ef87033b1fb4576ed13b1b83cf00c6fb12b917d
akshat12000 <akshatbhatnagar174@gmail.com>
2022-07-07 13:23:56 +0530

    Changes in frontend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const blogRouter = require("./routes/blogRoutes");
 const session = require("express-session");
 const passport = require("passport");
@@ -10,7 +10,7 @@ const path= require("path");
 const app = express();
 
 app.use(express.json())
-// app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin: 'https://mern-blogit.herokuapp.com/'}));
 
 app.use(session({
     secret:process.env.PASSPORT_SECRET,
